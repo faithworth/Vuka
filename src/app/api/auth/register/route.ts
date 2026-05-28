e3b98b01630cd33d752aae9b9e0e982b58aa1206
faithworth <intolerantino@gmail.com>
2026-05-28 12:29:55 +0200

    Add files via upload

--- src/app/api/auth/register/route.ts
+++ src/app/api/auth/register/route.ts
@@ -33,8 +33,8 @@ export async function POST(req: NextRequest) {
       await prisma.industryUser.create({
         data: {
           userId: user.id,
-          company: company || '',
-          position: position || '',
+          companyName: company || '',
+          role: position || '',
         },
       });
     }
